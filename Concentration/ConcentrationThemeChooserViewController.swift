413ed22f280dc3bc4b0031894673edaf0cbb1f0d
miretz <miretz@gmail.com>
2018-01-21 21:51:22 +0100

    Multiple MVCs (Lecture 7)

---
+++ Concentration/ConcentrationThemeChooserViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ConcentrationThemeChooserViewController.swift
+//  Concentration
+//
+//  Created by Miretz Dev on 21/01/2018.
+//  Copyright © 2018 Miretz. All rights reserved.
+//
+
+import UIKit
+
+class ConcentrationThemeChooserViewController: UIViewController, UISplitViewControllerDelegate {
+
+    let themes = [
+        "Halloween": Theme.halloween.rawValue,
+        "Devices": Theme.devices.rawValue,
+        "Food": Theme.food.rawValue,
+        "Emojis": Theme.emojis.rawValue,
+        "Sports": Theme.sports.rawValue,
+        "People": Theme.people.rawValue,
+        "Animals": Theme.animals.rawValue,
+        "Flags": Theme.flags.rawValue,
+    ]
+    
+    override func awakeFromNib() {
+        splitViewController?.delegate = self
+    }
+    
+    func splitViewController(_ splitViewController: UISplitViewController,
+        collapseSecondary secondaryViewController: UIViewController,
+        onto primaryViewController: UIViewController
+    ) -> Bool {
+        if let cvc = secondaryViewController as? ConcentrationViewController {
+            if cvc.theme == nil {
+                return true
+            }
+        }
+        return false
+    }
+    
+    private func checkAndGetTheme(themeName: String) -> String {
+        if let theme = themes[themeName]{
+            return theme
+        } else {
+            return Theme.randomTheme().rawValue
+        }
+    }
+    
+    
+    @IBAction func changeTheme(_ sender: Any) {
+        if let cvc = splitViewDetailConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle {
+                cvc.theme = checkAndGetTheme(themeName: themeName)
+            }
+        } else if let cvc = lastSeguedToViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle {
+                cvc.theme = checkAndGetTheme(themeName: themeName)
+            }
+            navigationController?.pushViewController(cvc, animated: true)
+        } else {
+            performSegue(withIdentifier: "Choose Theme", sender: sender)
+        }
+    }
+    
+    private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
+        return splitViewController?.viewControllers.last as? ConcentrationViewController
+    }
+
+    // MARK: - Navigation
+    
+    private var lastSeguedToViewController : ConcentrationViewController?
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Choose Theme" {
+            if let cvc = segue.destination as? ConcentrationViewController {
+                if let themeName = (sender as? UIButton)?.currentTitle {
+                    cvc.theme = checkAndGetTheme(themeName: themeName)
+                    lastSeguedToViewController = cvc
+                }
+            }
+        }
+    }
+
+}
--- Concentration/ConcentrationViewController.swift
+++ Concentration/ConcentrationViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class ConcentrationViewController: UIViewController {
 
     private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     
@@ -27,7 +27,7 @@ final class ViewController: UIViewController {
     private func updateScoreLabel(){
         let attributes: [NSAttributedStringKey: Any] = [
             .strokeWidth: 5.0,
-            .strokeColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            .strokeColor: #colorLiteral(red: 0.370555222, green: 0.3705646992, blue: 0.3705595732, alpha: 1)
         ]
         let text = (game.isGameOver) ? "Well done! Score: \(game.score)" : "Score: \(game.score)"
         flipCountLabel.attributedText = NSAttributedString(string: text, attributes: attributes)
@@ -50,18 +50,30 @@ final class ViewController: UIViewController {
     }
     
     private func updateViewFromModel(){
-        for index in cardButtons.indices {
-            let button = cardButtons[index]
-            let card = game.cards[index]
-            if card.isFaceUp {
-                button.setTitle(emoji(for: card), for: UIControlState.normal)
-                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            } else {
-                button.setTitle("", for: UIControlState.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        if cardButtons != nil {
+            for index in cardButtons.indices {
+                let button = cardButtons[index]
+                let card = game.cards[index]
+                if card.isFaceUp {
+                    button.setTitle(emoji(for: card), for: UIControlState.normal)
+                    button.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+                } else {
+                    button.setTitle("", for: UIControlState.normal)
+                    button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0, green: 0.3285208941, blue: 0.5748849511, alpha: 1)
+                }
             }
         }
     }
+    
+    
+    var theme: String? {
+        didSet {
+            emojiChoices = theme ?? ""
+            emoji = [:]
+            updateViewFromModel()
+        }
+    }
+    
     
     private var emojiChoices = Theme.randomTheme().rawValue
 
